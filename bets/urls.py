53a746b7bedf511ee39b43df14f4d9bc97d26277
Manel Vilar <manelvf@gmail.com>
2025-03-26 18:24:28 +0000

    Add Pillow library

--- bets/urls.py
+++ bets/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("/about", views.about, name="about"),
+    path("about", views.about, name="about"),
     path("service1", views.service1, name="service1"),
     path("service2", views.service2, name="service2"),
     path("all_services", views.all_services, name="all_services"),
